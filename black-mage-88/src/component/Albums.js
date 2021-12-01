f5bbed608ffb6b04b5aa70e23adb89f03491d80f
robertthome <remedy42290@gmail.com>
2021-11-30 22:48:56 -0800

    fixing home img issue: centering img

---
+++ black-mage-88/src/component/Albums.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './compStyles/Albums.css'
+
+const Albums = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  )
+}
+
+export default Albums
--- black-mage-88/src/pages/Home.js
+++ black-mage-88/src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import './pagesStlyes/Home.css'
 import Virgo from '../assets/Virgo.jpeg'
+import Albums from '../component/Albums'
 
 const Home = () => {
   return (
     <div className="home-container">
       <h1 className="title">Black Mage 88</h1>
       <img className="home-img" src={Virgo} alt="virgo" />
+      <Albums />
     </div>
   )
 }
