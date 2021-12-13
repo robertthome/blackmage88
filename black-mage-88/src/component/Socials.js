c652a449db6e05278e4cf34c97c7f729efbe3e68
robertthome <remedy42290@gmail.com>
2021-12-12 23:33:14 -0800

    soundcloud added facebook deleted target blank

--- black-mage-88/src/component/Socials.js
+++ black-mage-88/src/component/Socials.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import './compStyles/Socials.css'
 
-import { FaSpotify, FaInstagram, FaFacebookSquare } from 'react-icons/fa'
-// import { AiFillMail } from 'react-icons/ai'
+import { FaSpotify, FaInstagram, FaSoundcloud } from 'react-icons/fa'
 import { SiApplemusic } from 'react-icons/si'
 
 const Socials = () => {
   return (
     <div className="socials-container">
-      <a href="https://open.spotify.com/artist/2i0qV3n6HvZUhUfXMBplse">
+      <a href="https://open.spotify.com/artist/2i0qV3n6HvZUhUfXMBplse?si=67D5bqKNSI-EAkE2IGR9Dg" target="_blank">
         <FaSpotify className="social" />
       </a>
-      <a href="https://music.apple.com/us/search?term=black%20mage%2088">
+      <a href="https://music.apple.com/us/search?term=black%20mage%2088" target="_blank">
         <SiApplemusic className="social" />
       </a>
-      <a href="https://www.instagram.com/mic_key_theblackmage/">
+      <a href="https://www.instagram.com/mic_key_theblackmage/" target="_blank">
         <FaInstagram className="social" />
       </a>
-      <a href="https://www.facebook.com/mickey.prigatano">
-        <FaFacebookSquare className="social" />
+      <a href='https://soundcloud.com/user-703614296' target="_blank">
+        <FaSoundcloud className='social' />
       </a>
     </div>
   )
