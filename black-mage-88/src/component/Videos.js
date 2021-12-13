00bb0fe1cb3771077e6aad649f7068e614126e2e
robertthome <remedy42290@gmail.com>
2021-12-12 23:24:56 -0800

    single video responsiveness video page

--- black-mage-88/src/component/Videos.js
+++ black-mage-88/src/component/Videos.js
@@ -1,51 +1,71 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactPlayer from 'react-player'
 
-const Videos = () => {
-  return (
-    <div className="video-container">
-      <ReactPlayer
-        className="react-player"
-        url="https://www.youtube.com/watch?v=DfbKZwWzlLM"
-        controls={true}
-      />
-      <ReactPlayer
-        className="react-player"
-        url="https://www.youtube.com/watch?v=Dt6bgxxbNzU&t=71s"
-        controls={true}
-      />
-      <ReactPlayer
-        className="react-player"
-        url="https://www.youtube.com/watch?v=v4v6ImsP3Ok"
-        controls={true}
-      />
-      <ReactPlayer
-        className="react-player"
-        url="https://www.youtube.com/watch?v=46ETV8-Xj-4"
-        controls={true}
-      />
-      <ReactPlayer
-        className="react-player"
-        url="https://www.youtube.com/watch?v=WLOwEhgVlmQ"
-        controls={true}
-      />
-      <ReactPlayer
-        className="react-player"
-        url="https://www.youtube.com/watch?v=L2PcMYwhJuk&t=23s"
-        controls={true}
-      />
-      <ReactPlayer
-        className="react-player"
-        url="https://www.youtube.com/watch?v=1XVIclIjRcc"
-        controls={true}
-      />
-      <ReactPlayer
-        className="react-player"
-        url="https://www.youtube.com/watch?v=EZmWhoUAF0Y&t=112s"
-        controls={true}
-      />
-    </div>
-  )
+import '../component/compStyles/Videos.css'
+
+class Videos extends Component {
+  render() {
+    return (
+      <div className="video-container">
+        <ReactPlayer
+          className="react-player"
+          url="https://www.youtube.com/watch?v=DfbKZwWzlLM"
+          controls={true}
+          width="100%"
+          height="100%"
+        />
+      </div>
+    )
+  }
 }
+// {/* <ReactPlayer
+// className="react-player marv"
+// url="https://www.youtube.com/watch?v=Dt6bgxxbNzU&t=71s"
+// controls={true}
+// width="100%"
+// height="100%"
+// />
+// <ReactPlayer
+// className="react-player"
+// url="https://www.youtube.com/watch?v=v4v6ImsP3Ok"
+// controls={true}
+// width="100%"
+// height="100%"
+// />
+// <ReactPlayer
+// className="react-player"
+// url="https://www.youtube.com/watch?v=46ETV8-Xj-4"
+// controls={true}
+// width="100%"
+// height="100%"
+// />
+// <ReactPlayer
+// className="react-player"
+// url="https://www.youtube.com/watch?v=WLOwEhgVlmQ"
+// controls={true}
+// width="100%"
+// height="100%"
+// />
+// <ReactPlayer
+// className="react-player"
+// url="https://www.youtube.com/watch?v=L2PcMYwhJuk&t=23s"
+// controls={true}
+// width="100%"
+// height="100%"
+// />
+// <ReactPlayer
+// className="react-player"
+// url="https://www.youtube.com/watch?v=EZmWhoUAF0Y&t=112s"
+// controls={true}
+// width="100%"
+// height="100%"
+// />
+// <ReactPlayer
+// className="react-player"
+// url="https://www.youtube.com/watch?v=1XVIclIjRcc"
+// controls={true}
+// width="100%"
+// height="100%"
+// /> */}
 
 export default Videos
