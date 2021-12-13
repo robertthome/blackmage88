5a0fc61d134e30367c6a1a1bbf48e2f7bf17666e
robertthome <remedy42290@gmail.com>
2021-12-12 23:47:06 -0800

    target blank erases

--- black-mage-88/src/component/Socials.js
+++ black-mage-88/src/component/Socials.js
@@ -7,16 +7,16 @@ import { SiApplemusic } from 'react-icons/si'
 const Socials = () => {
   return (
     <div className="socials-container">
-      <a href="https://open.spotify.com/artist/2i0qV3n6HvZUhUfXMBplse?si=67D5bqKNSI-EAkE2IGR9Dg" target="_blank">
+      <a href="https://open.spotify.com/artist/2i0qV3n6HvZUhUfXMBplse?si=67D5bqKNSI-EAkE2IGR9Dg" >
         <FaSpotify className="social" />
       </a>
-      <a href="https://music.apple.com/us/search?term=black%20mage%2088" target="_blank">
+      <a href="https://music.apple.com/us/search?term=black%20mage%2088" >
         <SiApplemusic className="social" />
       </a>
-      <a href="https://www.instagram.com/mic_key_theblackmage/" target="_blank">
+      <a href="https://www.instagram.com/mic_key_theblackmage/" >
         <FaInstagram className="social" />
       </a>
-      <a href='https://soundcloud.com/user-703614296' target="_blank">
+      <a href='https://soundcloud.com/user-703614296' >
         <FaSoundcloud className='social' />
       </a>
     </div>
